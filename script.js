ad84eab24e63304776a4b0a11e0594b33868df9e
nuttylmao <76841561+nuttylmao@users.noreply.github.com>
2025-02-02 22:46:40 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -5,235 +5,127 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
-const client_id = urlParams.get("client_id") || "";
-const client_secret = urlParams.get("client_secret") || "";
-let refresh_token = urlParams.get("refresh_token") || "";
+const code = urlParams.get("code") || "";
+
+const baseURL = "https://nuttylmao.github.io/spotify-widget";
+const redirect_uri = `${baseURL}/configure`;
+let refresh_token = "";
 let access_token = "";
-
-const visibilityDuration = urlParams.get("duration") || 0;
-const hideAlbumArt = urlParams.has("hideAlbumArt");
-
-let currentState = false;
+let browserSourceURL = "";
 
 
-/////////////////
-// SPOTIFY API //
-/////////////////
 
-// Update the access token - this expires so needs to be refreshed with refresh_token
-async function RefreshAccessToken() {
-	console.debug(`Client ID: ${client_id}`);
-	console.debug(`Client Secret: ${client_secret}`);
-	console.debug(`Refresh Token: ${refresh_token}`);
+/////////////////////////
+// AUTHORIZATION STUFF //
+/////////////////////////
 
-    let body = "grant_type=refresh_token";
-    body += "&refresh_token=" + refresh_token;
+function RequestAuthorization() {
+    const client_id = document.getElementById("client_id_box").value;
+    const client_secret = document.getElementById("client_secret_box").value;
+    localStorage.setItem("client_id", client_id);
+    localStorage.setItem("client_secret", client_secret);
+
+    let url = "https://accounts.spotify.com/authorize";
+    url += "?client_id=" + client_id;
+    url += "&response_type=code";
+    url += "&redirect_uri=" + encodeURI(redirect_uri);
+    url += "&show_dialog=true";
+    url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
+    window.location.href = url;
+}
+
+// If there is no code in the query string, direct the user to authorize their account
+if (code != "") {
+    FetchAccessToken(code);
+}
+else {
+    document.getElementById("connectBox").style.display = 'inline';
+}
+
+async function FetchAccessToken(code) {
+    const client_id = localStorage.getItem("client_id");
+    const client_secret = localStorage.getItem("client_secret");
+    console.debug(`Client ID: ${client_id}`);
+    console.debug(`Client Secret: ${client_secret}`);
+
+    let body = "grant_type=authorization_code";
+    body += "&code=" + code;
+    body += "&redirect_uri=" + encodeURI(redirect_uri);
     body += "&client_id=" + client_id;
+    body += "&client_secret=" + client_secret;
+    console.log(body);
 
-	const response = await fetch("https://accounts.spotify.com/api/token", {
-		method: "POST",
-		headers: {
-			'Authorization': `Basic ${btoa(client_id + ":" + client_secret)}`,
-			'Content-Type': 'application/x-www-form-urlencoded'
-		},
-		body: body
-	});
+    // Get the current player information from Spotify
+    const response = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: {
+            'Authorization': `Basic ${btoa(client_id + ":" + client_secret)}`,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: body
+    });
 
-	// If we got a response, save the access token
+    // If we got a response, save the access token
 	if (response.ok)
-	{
-		const responseData = await response.json();
-		console.debug(responseData);
-		//refresh_token = responseData.refresh_token;			// Unsure if we need to replace the refresh_token but do it just in case
-		access_token = responseData.access_token;			// Save access token for all future API calls
-	}
-	else
-	{
-		console.error(`${response.status}`);
-	}
-}
+    {
+        const responseData = await response.json();
+        console.debug(responseData);
+        refresh_token = responseData.refresh_token;			// Unsure if we need to replace the refresh_token but do it just in case
+        access_token = responseData.access_token;			// Save access token for all future API calls
 
-async function GetCurrentlyPlaying(refreshInterval) {
-	try {
-		// Get the current player information from Spotify
-		const response = await fetch("https://api.spotify.com/v1/me/player/currently-playing", {
-			method: "GET",
-			headers: {
-				'Authorization': `Bearer ${access_token}`,
-				'Content-Type': 'application/json'
-			}
-		})
-	
-		// If we got a response, save the access token
-		if (response.ok)
-		{
-			const responseData = await response.json();
-			console.debug(responseData);
-			UpdatePlayer(responseData);
-		}
-		else
-		{
-			switch (response.status)
-			{
-				case 401:
-					console.debug(`${response.status}`)
-					RefreshAccessToken();
-					break;
-				default:
-					console.error(`${response.status}`)
-			}
-		}
-		// Refresh
-		setTimeout(() => {
-			GetCurrentlyPlaying()
-		}, 1000);
-	}
-	catch (error)
-	{
-		console.debug(error);
-		SetVisibility(false);
-		
-		// Try again in 2 seconds
-		setTimeout(() => {
-			GetCurrentlyPlaying()
-		}, 2000);
-	}
-}
-
-function UpdatePlayer(data) {
-	const isPlaying = data.is_playing;							// The play/pause state of the player
-	const albumArt = `${data.item.album.images[0].url}`;		// The album art URL
-	const artist = `${data.item.artists[0].name}`;				// Name of the artist
-	const name = `${data.item.name}`;							// Name of the song
-	const duration = `${data.item.duration_ms/1000}`;			// The duration of the song in seconds
-	const progress = `${data.progress_ms/1000}`;				// The current position in seconds
-
-	// Set the visibility of the player, but only if the state is different than the last time we checked
-	if (isPlaying != currentState) {
-
-		// Set player visibility
-		if (!isPlaying)
-		{
-			console.debug("Hiding player...");
-			SetVisibility(false);
-		}
-		else
-		{
-			console.debug("Showing player...");
-			setTimeout(() => {
-				SetVisibility(true);
-			}, 500);
-		}
-	}
-
-	// Set thumbnail
-	UpdateAlbumArt(document.getElementById("albumArt"), albumArt);
-	UpdateAlbumArt(document.getElementById("backgroundImage"), albumArt);
-
-	// Set song info
-	UpdateTextLabel(document.getElementById("artistLabel"), artist);
-	UpdateTextLabel(document.getElementById("songLabel"), name);
-	
-	// Set progressbar
-	const progressPerc = ((progress / duration) * 100);			// Progress expressed as a percentage
-	const progressTime = ConvertSecondsToMinutesSoThatItLooksBetterOnTheOverlay(progress);
-	const timeRemaining = ConvertSecondsToMinutesSoThatItLooksBetterOnTheOverlay(duration - progress);
-	console.debug(`Progress: ${progressTime}`);
-	console.debug(`Time Remaining: ${timeRemaining}`);
-	document.getElementById("progressBar").style.width = `${progressPerc}%`;
-	document.getElementById("progressTime").innerHTML = progressTime;
-	document.getElementById("timeRemaining").innerHTML = `-${timeRemaining}`;
-
-	setTimeout(() => {
-		document.getElementById("albumArtBack").src = albumArt;
-		document.getElementById("backgroundImageBack").src = albumArt;
-	}, 1000);
-}
-
-function UpdateTextLabel(div, text) {
-	if (div.innerText != text) {
-		div.setAttribute("class", "text-fade");
-		setTimeout(() => {
-			div.innerText = text;
-			div.setAttribute("class", ".text-show");
-		}, 500);
-	}
-}
-
-function UpdateAlbumArt(div, imgsrc) {
-	if (div.src != imgsrc) {
-		div.setAttribute("class", "text-fade");
-		setTimeout(() => {
-			div.src = imgsrc;
-			div.setAttribute("class", "text-show");
-		}, 500);
-	}
+        browserSourceURL = `${baseURL}?client_id=${client_id}&client_secret=${client_secret}&refresh_token=${refresh_token}`;
+        document.getElementById("authorizationBox").style.display = 'inline';
+    }
+    else
+    {
+        console.error(`${response.status}`);
+    }
 }
 
 
 
-//////////////////////
-// HELPER FUNCTIONS //
-//////////////////////
+///////////////////////////
+// BUTTON CLICK HANDLERS //
+///////////////////////////
 
-function ConvertSecondsToMinutesSoThatItLooksBetterOnTheOverlay(time) {
-	const minutes = Math.floor(time / 60);
-	const seconds = Math.trunc(time - minutes * 60);
+const clientIdBox = document.getElementById('client_id_box');
+const clientSecretBox = document.getElementById('client_secret_box');
+const authorizeButton = document.getElementById('authorizeButton');
 
-	return `${minutes}:${('0' + seconds).slice(-2)}`;
+// Function to check if either input is empty
+function checkInputs() {
+    if (clientIdBox.value.trim() === '' || clientSecretBox.value.trim() === '') {
+        authorizeButton.disabled = true;    // Disable the button
+    } else {
+        authorizeButton.disabled = false;   // Enable the button
+    }
 }
 
-function SetVisibility(isVisible) {
-	widgetVisibility = isVisible;
+// Listen for changes in the input boxes
+clientIdBox.addEventListener('input', checkInputs);
+clientSecretBox.addEventListener('input', checkInputs);
 
-	const mainContainer = document.getElementById("mainContainer");
+// Initial check when the page loads, just in case
+checkInputs();
 
-	if (isVisible) {
-		mainContainer.style.opacity = 1;
-		mainContainer.style.bottom = "50%";
-	}
-	else {
-		mainContainer.style.opacity = 0;
-		mainContainer.style.bottom = "calc(50% - 20px)";
-	}
-	currentState = isVisible;
+function CopyToURL() {
+    navigator.clipboard.writeText(browserSourceURL);
+
+    document.getElementById("copyURLButton").innerText = "Copied to clipboard";
+    document.getElementById("copyURLButton").style.backgroundColor = "#00dd63"
+    document.getElementById("copyURLButton").style.color = "#ffffff";
+
+    setTimeout(() => {
+        document.getElementById("copyURLButton").innerText = "Click to copy URL";
+        document.getElementById("copyURLButton").style.backgroundColor = "#ffffff";
+        document.getElementById("copyURLButton").style.color = "#181818";
+    }, 3000);
 }
 
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// RESIZER THING BECAUSE I THINK I KNOW HOW RESPONSIVE DESIGN WORKS EVEN THOUGH I DON'T //
-//////////////////////////////////////////////////////////////////////////////////////////
-
-let outer = document.getElementById('mainContainer'),
-	maxWidth = outer.clientWidth,
-	maxHeight = outer.clientHeight;
-
-window.addEventListener("resize", resize);
-
-resize();
-function resize() {
-	const scale = window.innerWidth / maxWidth;
-	outer.style.transform = 'translate(-50%, 50%) scale(' + scale + ')';
+function OpenInstructions() {
+    window.open("https://nuttylmao.notion.site/Spotify-Widget-18e19969b237807ca88cfc9c4159da15", '_blank').focus();
 }
 
-
-
-/////////////////////////////////////////////////////////////////////
-// IF THE USER PUT IN THE HIDEALBUMART PARAMATER, THEN YOU SHOULD  //
-//   HIDE THE ALBUM ART, BECAUSE THAT'S WHAT IT'S SUPPOSED TO DO   //
-/////////////////////////////////////////////////////////////////////
-
-if (hideAlbumArt) {
-	document.getElementById("albumArtBox").style.display = "none";
-	document.getElementById("songInfoBox").style.width = "calc(100% - 20px)";
+function OpenDonationPage() {
+    window.open("http://nutty.gg/pages/donate", '_blank').focus();
 }
-
-
-
-////////////////////////////////
-// KICK OFF THE WHOLE WIDGET  //
-////////////////////////////////
-
-RefreshAccessToken();
-GetCurrentlyPlaying();			// This is a recursive function, so just run it once
